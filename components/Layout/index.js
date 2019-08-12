90353be04d3297780007aef3760bbd9f6b462d13
Leon Weber <lw.leonweber@gmail.com>
2019-08-12 17:35:01 +0200

    Add Google Analytics

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,5 +1,9 @@
 // Essentials
+import React from 'react';
 import Head from 'next/head';
+
+// Utils
+import { initGA, logPageView } from '../../utils/analytics'
 
 // Components
 import Header from '../Header';
@@ -8,16 +12,29 @@ import Header from '../Header';
 import style from '../../scss/style.scss';
 import layoutStyle from './Layout.scss';
 
-const Layout = (props) => (
-  <div className={layoutStyle.Wrapper}>
-    <Head>
-      <title key="title">Verónica Vicente Storm</title>
-      <link rel="stylesheet" href="https://cdn.plyr.io/3.5.6/plyr.css" />
-      <link rel="stylesheet" href="https://use.typekit.net/tds7bft.css" />
-    </Head>
-    <Header />
-    {props.children}
-  </div>
-);
+class Layout extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+
+    logPageView();
+  }
+
+  render() {
+    return (
+      <div className={layoutStyle.Wrapper}>
+        <Head>
+          <title key="title">Verónica Vicente Storm</title>
+          <link rel="stylesheet" href="https://cdn.plyr.io/3.5.6/plyr.css" />
+          <link rel="stylesheet" href="https://use.typekit.net/tds7bft.css" />
+        </Head>
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 export default Layout;
